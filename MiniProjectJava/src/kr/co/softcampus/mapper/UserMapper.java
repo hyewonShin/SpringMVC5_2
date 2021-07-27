c65a83f14b50bfd7f2c4e0758b9579b6be7ab11f
hyewonShin <rusy7225@naver.com>
2021-07-27 19:26:24 +0900

    회원가입을 위해 입력한 내용을 저장하는 처리를 함(ajax사용)

--- MiniProjectJava/src/kr/co/softcampus/mapper/UserMapper.java
+++ MiniProjectJava/src/kr/co/softcampus/mapper/UserMapper.java
@@ -1,6 +1,9 @@
 package kr.co.softcampus.mapper;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
+
+import kr.co.softcampus.beans.UserBean;
 
 public interface UserMapper {
 
@@ -9,6 +12,13 @@ public interface UserMapper {
 			" where user_id = #{user_id}")
 	String checkUserIdExist(String user_id);
 	//아랫줄로 내린 경우 꼭!! 한 칸씩 띄어주기(안 띄어주면 쿼리문 오류라고 에러남)
-}
 
+
+	@Insert("insert into user_table (user_idx, user_name, user_id, user_pw) " +
+			"values (user_seq.nextval, #{user_name}, #{user_id}, #{user_pw})")
+	void addUserInfo(UserBean joinUserBean);
+	
+}	
+	
+	
 //Mapper는 Interpace클래스로 만들고, Servlet에  bean으로 Mapper 등록해준다.
